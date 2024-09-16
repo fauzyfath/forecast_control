aa2f8b6180f4e84c3bfd9edbb99511392014bdc8
MuhammadAlifMaulanaR <muhalif.maulana17@gmail.com>
2024-09-16 16:20:44 +0800

    Update file BH1750.py menambahkan intensitas cahaya malam < 1

--- BH1750.py
+++ BH1750.py
@@ -3,13 +3,13 @@ import time
 from bh1750 import BH1750
 
 # Membuat bus I2C
-bus = smbus.SMBus(1)  # Pada Raspberry Pi, biasanya menggunakan I2C-1
+bus = smbus.SMBus(1)  # Pada Raspberry Pi, biasanya I2C-1
 
 # Inisialisasi sensor BH1750
 sensor = BH1750(bus)
 
 try:
-    # Mengatur mode ke CONTINUOUS_HIGH_RES_MODE dengan alamat 0x23
+    # Mengatur mode ke CONTINUOUS_HIGH_RES_MODE
     sensor.power_on()
     sensor.reset()
     
@@ -19,11 +19,13 @@ try:
         # Membaca tingkat pencahayaan
         lux = sensor.lux()
 
-        # Cek apakah pembacaan berhasil
-        if lux > 50:
-            print(f"Cerah: {lux:.2f} lux")
-        elif lux < 50:
+        # Cek kondisi pencahayaan
+        if lux > 1 and lux < 50:
             print(f"Mendung: {lux:.2f} lux")
+        elif lux > 50:
+            print(f"Cerah: {lux:.2f} lux")
+        elif lux <= 1:
+            print(f"Malam: {lux:.2f} lux")
         else:
             print("Error reading light level")
         
